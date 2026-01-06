47d4f18fef1bdf26f344bbff0b01d7f0a8a11add
Махвойд <121610661+Mahmoud-Al-Kowatli@users.noreply.github.com>
2026-01-06 22:30:55 +0300

    Create Order.h

---
+++ FactoryManager/Order.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Libraries.h"
+
+class Order {
+public:
+    int id;
+    int priority;
+    time_t arrivalTime;
+    int requiredQuantity;
+    int clientID;
+    int productID;
+    double totalValue;
+
+    void calculateValue();
+    
+    bool operator<(const Order& other) const {
+        return this->priority < other.priority;
+    }
+};
