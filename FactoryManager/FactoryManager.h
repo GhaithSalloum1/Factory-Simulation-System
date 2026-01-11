8be8f8106dec6a61a44e1f99fbf02c356146b90f
MajedAnka <majedanka444@gmail.com>
2026-01-11 23:04:38 +0200

    edited the factoryManager

--- FactoryManager/FactoryManager.h
+++ FactoryManager/FactoryManager.h
@@ -1,18 +1,26 @@
 #pragma once
-#include"Libraries.h"
+#include "Libraries.h"
+#include "OrdersManager.h"
+#include "WarehouseManager.h"
+#include "ProductionFloor.h"
 
 class FactoryManager
 {
 private:
-
-
 public:
 	FactoryManager();
 	~FactoryManager();
-
-
+	OrdersManager orderManager;
+	WarehouseManager warehouseManager;
+	ProductionFloor productionFloor;
 	void runSimulation();
-
-
+	int AddOrder();
+	void editOrder(int id);
+	void deleteOrder();
+	void showHistory();
+	bool checkIfOrderExist(int id);
+	Product choosingProduct();
+	int checkIfNumber();
+	int checkIfNumber(int x, int y);
+	Order::Priority setPriority();
 };
-
