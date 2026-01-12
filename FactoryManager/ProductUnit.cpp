a0f485f6282981e8c678711eb0e2a26735858326
Hussam Al Din Al Kial <hussamaldinallkial@gmail.com>
2026-01-12 20:51:38 +0300

    Edit on a ProductUnit function called getProductDetails for not to go on logical error

--- FactoryManager/ProductUnit.cpp
+++ FactoryManager/ProductUnit.cpp
@@ -1,14 +1,17 @@
 #include "ProductUnit.h"
 
 
-int ProductUnit::counter = 1;
+int ProductUnit::counter = 0;
 
 ProductUnit::ProductUnit() : unitID(0), parentOrderID(0), isFinished(false) {}
 
-void ProductUnit::addEvent(string desc){
+void ProductUnit::addEvent(string desc)
+{
     history.push_back(Event(desc));
 }
-ProductUnit::ProductUnit(int parentOrderID){
+
+ProductUnit::ProductUnit(int parentOrderID)
+{
     unitID = ++counter;
     this->parentOrderID = parentOrderID;
     isFinished = false;
